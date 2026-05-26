7ae3eb83b720d1aecbd87f37531444381877839d
Xepoiis <jose.galdamez.borquez@gmail.com>
2026-05-26 12:33:09 -0400

    no me acuerdo q wea hice

--- js/formulario.js
+++ js/formulario.js
@@ -1,6 +1,14 @@
-let archivo = document
-
 window.onload = function () {
-    let camponombre = document.getElementById('input_nombre')
-    console.log(camponombre)
+}
+
+function validarFormulario() {
+    alert("Cargando funcion de validacion de formulario...");
+    let datoNombre = document.getElementById('input_nombre');
+    let datoCorreo = document.getElementById('input_correo');
+    let datoContrasena = document.getElementById('input_contrasena');
+    let datoRepetirContrasena = document.getElementById('input_rep_contrasena');
+    let datoNacimiento = document.getElementById('input_nacimiento');
+    let datoFoto = document.getElementById('input_foto');
+    
+    console.log(datoNombre.value +'\n'+ datoCorreo.value +'\n'+ datoContrasena.value +'\n'+ datoRepetirContrasena.value +'\n'+ datoNacimiento.value +'\n'+ datoFoto.value);
 }
