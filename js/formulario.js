10409d19fb7693732e7d9df5b594f1b9d2a678d1
xepoiis <jose.galdamez.borquez@gmail.com>
2026-05-28 12:16:10 -0400

    Agrega el tema Bootstrap Slate y ajusta la validación del formulario de registro.

--- js/formulario.js
+++ js/formulario.js
@@ -2,13 +2,52 @@ window.onload = function () {
 }
 
 function validarFormulario() {
-    alert("Cargando funcion de validacion de formulario...");
     let datoNombre = document.getElementById('input_nombre');
     let datoCorreo = document.getElementById('input_correo');
     let datoContrasena = document.getElementById('input_contrasena');
     let datoRepetirContrasena = document.getElementById('input_rep_contrasena');
+    let datoGenero = document.getElementById('selectGenero');
     let datoNacimiento = document.getElementById('input_nacimiento');
-    let datoFoto = document.getElementById('input_foto');
+    let datoArchivo = document.getElementById('input_foto');
     
-    console.log(datoNombre.value +'\n'+ datoCorreo.value +'\n'+ datoContrasena.value +'\n'+ datoRepetirContrasena.value +'\n'+ datoNacimiento.value +'\n'+ datoFoto.value);
+    if(datoNombre.value == ''){
+        datoNombre.classList.add('is-invalid');
+    }else{
+        datoNombre.classList.remove('is-invalid');
+        datoNombre.classList.add('is-valid');}
+
+    if(datoCorreo.value == '' || !validarCorreo(datoCorreo.value)){
+        datoCorreo.classList.add('is-invalid');
+    }else{
+        datoCorreo.classList.remove('is-invalid');
+        datoCorreo.classList.add('is-valid');}
+
+    if(datoContrasena.value == ''){
+        datoContrasena.classList.add('is-invalid');
+    }else{
+        datoContrasena.classList.remove('is-invalid');
+        datoContrasena.classList.add('is-valid');}
+
+    if(datoRepetirContrasena.value == ''){
+        datoRepetirContrasena.classList.add('is-invalid');
+    }else{
+        datoRepetirContrasena.classList.remove('is-invalid');
+        datoRepetirContrasena.classList.add('is-valid');}
+
+    if(datoNacimiento.value == ''){
+        datoNacimiento.classList.add('is-invalid');
+    }else{
+        datoNacimiento.classList.remove('is-invalid');
+        datoNacimiento.classList.add('is-valid');}
+
+    if(datoGenero.value == ''){
+        datoGenero.classList.add('is-invalid');
+    }else{
+        datoGenero.classList.remove('is-invalid');
+        datoGenero.classList.add('is-valid');}
+}
+
+function validarCorreo(email){
+    const expression = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return expression.test(email);
 }
